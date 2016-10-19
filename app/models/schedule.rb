866798e7c1f52c61407cbeee0c880af3ce23a224
Jose Trinidad Espinoza Gonzalez <trinoeg8@gmail.com>
2016-10-19 09:43:01 -0500

    - Schedule model:   * Add validation for 'hour' to be required   * Set default scope to return ordered records by name - Talks model:   * Now talks belongs to a 'Schedule':   This was to facilitate the rest client drawing a table with the whole   MagmaConf schedule

---
+++ app/models/schedule.rb
@@ -0,0 +1,13 @@
+class Schedule < ActiveRecord::Base
+  has_many :talks
+
+  validates :hour, presence: true
+
+  before_save :set_name
+  default_scope -> { order('name asc') }
+
+  private
+    def set_name
+      self.name = self.hour.to_s(:time)
+    end
+end
--- app/models/talk.rb
+++ app/models/talk.rb
@@ -1,4 +1,7 @@
 class Talk < ActiveRecord::Base
   belongs_to :speaker
   belongs_to :event
+  belongs_to :schedule
+
+  default_scope -> { order('starts_at asc') }
 end
