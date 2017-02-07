13dcd1ab76c1af24c67cf15035ed9dccb9e22b71
Jose Trinidad Espinoza Gonzalez <trinoeg8@gmail.com>
2017-02-06 19:05:27 -0600

    Fix JSON structure in response

--- app/controllers/api/v1/rates_controller.rb
+++ app/controllers/api/v1/rates_controller.rb
@@ -2,9 +2,9 @@ class Api::V1::RatesController < Api::V1::ApiController
   def create 
     begin
       talk = Talk.find(params[:talk_id])
-      rate_talk = TalkRate.new(rate: params[:rate], comment: params[:comment], talk_id: talk.id) 
-      if rate_talk.save
-        render json: rate_talk, status: 201
+      talk_rate= TalkRate.new(rate: params[:rate], comment: params[:comment], talk_id: talk.id, user_name: params[:user_name]) 
+      if talk_rate.save
+        render json: { success: true, rate: talk_rate }, status: 201
       else
         puts working_time.errors.full_message
         render json: '{"success":false, "message":"Something went wrong"}', status: 505
